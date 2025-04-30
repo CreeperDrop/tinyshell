130c921541cc9f83fb673fdd77d39319e8576ecb
CreeperDrop <30409155+CreeperDrop@users.noreply.github.com>
2025-05-01 00:48:28 +0300

    Added spacing

--- src/utils.c
+++ src/utils.c
@@ -42,3 +42,38 @@ int str_comp(char* str1, char* str2)
 
 	return !(*str1 == *str2);
 }
+
+void str_concat(char* str1, char* str2, char* concat)
+{
+	int i;
+	int j;
+
+	for (i = 0; str1[i] != '\0'; ++i)
+		concat[i] = str1[i];
+
+	for (j = 0; str2[j] != '\0'; ++j, ++i)
+		concat[i] = str2[j];
+	
+	concat[i] = '\0';
+	
+	return;
+}
+void str_copy(char* src, char* dest) {
+    while (*src) {
+        *dest++ = *src++;
+    }
+    *dest = '\0';
+}
+/*
+void str_copy(char* src, char* dest)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; ++i)
+		dest[i] = src[i];
+	
+	dest[i] = '\0';
+
+	return;
+}
+*/
